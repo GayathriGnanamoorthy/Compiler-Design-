a64af02ce62b7c948a5c6cf492ac932193a2a6ef
Gayathri Gnanamoorthy <71868720+GayathriGnanamoorthy@users.noreply.github.com>
2023-05-11 10:05:16 +0530

    Add files via upload

---
+++ copy1.py
@@ -0,0 +1,4 @@
+def original():
+    fake_name=["a","b","c"]
+    for i in range(fake_name):
+        print(i)
---
+++ copy2.py
@@ -0,0 +1,4 @@
+def copied():
+    fake_name_chnaged=["a","b","c"]
+    for j in range(fake_name_changed):
+        print(j)
